4d6a41b3df13986c2a46d11c8c9935aa5424547f
Matheus Fernandes <matheusfernandesm2411@gmail.com>
2026-03-27 11:30:42 -0300

    Formularios Retroalimentados

--- desafios/d004/conversao.php
+++ desafios/d004/conversao.php
@@ -28,6 +28,9 @@
         $padrao = numfmt_create("pt_BR", NumberFormatter::CURRENCY);
 
         echo "<p>Seus " . numfmt_format_currency($padrao, $real, "BRL") . " equivalem a <strong>" . numfmt_format_currency($padrao, $dolar, "USD") . "</strong></p>";
+        echo "<p><small>Fonte: Banco Central do Brasil</small></p>";
+
+
     ?>
     <button onclick="javascript:history.go(-1)">Voltar</button>
     </main>
